def9fecd9b0a56544431f5e419d69653ec6c861e
Simon Goodall <simon@simongoodall.co.uk>
2002-06-28 14:33:32 +0000

    Basic log functionality implemented

--- src/Log.cpp
+++ src/Log.cpp
@@ -3,39 +3,30 @@
 // Copyright (C) 2001 - 2002 Simon Goodall
 
 #include "Log.h"
+#include "iostream.h"
+
+/* LOG AIMS
+ * Will provide  ability to log each type to a different file
+ * FREQUENT LOG ACTIONS SHOULD BE IN A #if #endif BLOCK and
+ * only included in debug mode
+ */ 
 
 namespace Sear {
-
-Log::Log() : _logfile(cout)
-{
-
-}
-
-Log::Log(const std::ostream os) : _logfile(os)
-{
-
-}
-
-Log::~Log() {
-
-}
-
-bool Log::init() {
-  return true;
-}
-
-void Log::shutdown() {
-
-}
   
-void Log::changeLogFile(const std::ostream) {
-
+void Log::writeLog(const std::string &msg, LogLevel level) {
+  std::string type;
+  switch (level) {
+    case DEFAULT: type = "Default"; break;
+    case ERROR: type = "Error"; break;
+    case WARNING: type = "Warning"; break;
+    case METHOD_ENTRY: type = "Method Entry"; break;
+    case METHOD_EXIT: type = "Method Exit"; break;
+    case POSITION: type = "Position"; break;
+    case ERIS: type = "Eris"; break;		
+    case INFO: type = "Info"; break;
+  }
+  std::cout << "TIMESTAMP - " << type << ": " << msg << std::endl;
 }
-  
-void Log::writeLog(const std::string &, LogLevel) {
-
-}
-  
 
 } /* namespace Sear */
 
--- src/Log.h
+++ src/Log.h
@@ -5,31 +5,27 @@
 #ifndef _LOG_H_
 #define _LOG_H_ 1
 
-#include <iostream.h>
 #include <string>
 
 namespace Sear {
 
-typedef enum {
-  LOG_DEFAULT = 0,
-  LOG_ERRORS
-} LogLevel;
 	
 class Log {
 public:
-  Log();
-  Log(const std::ostream);
-  virtual ~Log();
+typedef enum {
+  DEFAULT = 0,
+  ERROR,
+  WARNING,
+  METHOD_ENTRY,
+  METHOD_EXIT,
+  POSITION,
+  ERIS,
+  INFO
+} LogLevel;
 
-  virtual bool init();
-  virtual void shutdown();
-  
-  virtual void changeLogFile(const std::ostream);
-  
-  virtual void writeLog(const std::string &, LogLevel);
+  static void writeLog(const std::string &, LogLevel);
   
 protected:
-  std::ostream _logfile;
 };
 
 } /* namespace Sear */
