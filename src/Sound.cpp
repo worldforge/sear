7b13408c9ac694c20bf78696315ff6bfbb47cd69
Simon Goodall <simon@simongoodall.co.uk>
2002-08-20 16:34:31 +0000

    20th August 2002, Simon Goodall  * Improved sound by changing from SDL-sound to SDL_mixer. This allows multiple sounds to be played at once

--- src/Sound.cpp
+++ src/Sound.cpp
@@ -4,106 +4,48 @@
 
 namespace Sear {
 
-Uint8 *Sound::decoded_ptr = NULL;
-Uint32 Sound::decoded_bytes = 0;
-int Sound::predecode = 0;
-int Sound::looping = 0;
-bool Sound::done_flag = false;
-  
 void Sound::init() {
   if (SDL_InitSubSystem(SDL_INIT_AUDIO) != 0) {
     std::cerr << SDL_GetError() << std::endl << std::flush;
     throw Exception("Error initialising SDL Sound Subsystem!");
   }
-  if (!Sound_Init()) {
-    std::cerr << Sound_GetError() << std::endl << std::flush;
-    throw Exception("Error initialising SDL Sound Library!");
+#define MONO (1)
+#define STEREO (2)
+ if (Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, STEREO, 4096) != 0) {
+    std::cerr << Mix_GetError() << std::endl << std::flush;
+    throw Exception("Error initialising SDL_mixer!");
   }
 }
 
 void Sound::shutdown() {
+  Mix_CloseAudio();
+  while(!sound_map.empty()) {
+    Mix_Chunk *sample = (sound_map.begin())->second;
+    if (sample) Mix_FreeChunk(sample);
+    sound_map.erase(sound_map.begin());
+  }
   SDL_QuitSubSystem(SDL_INIT_AUDIO);
 }
 
-void Sound::playsound(const std::string &file_name) {
-  Uint32 audio_buffersize = DEFAULT_AUDIOBUF;
-  Uint32 decode_buffersize = DEFAULT_DECODEBUF;
-  SDL_AudioSpec sdl_desired; 
-  static Sound_Sample *sample = NULL;
+Mix_Chunk *Sound::getSample(const std::string &file_name) {
+  Mix_Chunk *sample = NULL;
+  sample = sound_map[file_name];
+  if (sample) return sample;
+  sample=Mix_LoadWAV(file_name.c_str());
   if (sample) {
-    finish_playback(sample);
-    sample = NULL;
-    done_flag = false;
+    sound_map[file_name] = sample;
+    return sample;
   }
-  sample = Sound_NewSampleFromFile(file_name.c_str(), NULL, decode_buffersize);
-  if (!sample) {
-    throw Exception("Error loading file");
-  }
-  sdl_desired.freq = sample->actual.rate;
-  sdl_desired.format = sample->actual.format;
-  sdl_desired.channels = sample->actual.channels;
-
-  sdl_desired.samples = audio_buffersize;
-  sdl_desired.callback = audio_callback;
-  sdl_desired.userdata = sample;
-  if (SDL_OpenAudio(&sdl_desired, NULL) < 0) {
-    throw Exception("Error opening file");
-  }
-  SDL_PauseAudio(0);
+  printf("Mix_LoadWAV: %s\n", Mix_GetError());
+  return NULL;
+  
 }
 
-void Sound::finish_playback(Sound_Sample *sample) {
-  SDL_PauseAudio(1);
-  if (sample->flags & SOUND_SAMPLEFLAG_ERROR) {
-    fprintf(stderr, "Error in decoding sound file!\n reason: [%s].\n", Sound_GetError());
-  } /* if */
-  SDL_CloseAudio();  /* reopen with next sample's format if possible */
-  Sound_FreeSample(sample);
-  done_flag = true;
+void Sound::playsound(const std::string &file_name) {
+  // load sample.wav in to sample
+ Mix_Chunk *sample = getSample(file_name);
+ Mix_PlayChannel(-1, sample, 0);
 }
-
-void Sound::audio_callback(void *userdata, Uint8 *stream, int len) {
-  Sound_Sample *sample = (Sound_Sample *) userdata;
-  int bw = 0; /* bytes written to stream this time through the callback */
-  while (bw < len) {
-    int cpysize;  /* bytes to copy on this iteration of the loop. */
-    if (!read_more_data(sample)) { /* read more data, if needed. */
-       /* ...there isn't any more data to read! */
-       memset(stream + bw, '\0', len - bw);
-       done_flag = 1;
-  //     finish_playback(sample);
-       return;
-    } /* if */
-    /* decoded_bytes and decoder_ptr are updated as necessary... */
-    cpysize = len - bw;
-    if (cpysize > decoded_bytes) cpysize = decoded_bytes;
-    if (cpysize > 0) {
-      memcpy(stream + bw, decoded_ptr, cpysize);
-      bw += cpysize;
-      decoded_ptr += cpysize;
-      decoded_bytes -= cpysize;
-    } /* if */
-  } /* while */
-} /* audio_callback */
-
-
-int Sound::read_more_data(Sound_Sample *sample) {
-  if (done_flag)              /* probably a sigint; stop trying to read. */
-  decoded_bytes = 0;
-  if (decoded_bytes > 0)      /* don't need more data; just return. */
-  return(decoded_bytes);  
-  /* need more. See if there's more to be read... */
-  if (!(sample->flags & (SOUND_SAMPLEFLAG_ERROR | SOUND_SAMPLEFLAG_EOF))) {
-    decoded_bytes = Sound_Decode(sample);
-    decoded_ptr = (Uint8*)sample->buffer;
-    return(read_more_data(sample));  /* handle loops conditions. */
-  } /* if */
-  /* No more to be read from stream, but we may want to loop the sample. */
-  if (!looping) return(0);
-  /* we just need to point predecoded samples to the start of the buffer. */
-  Sound_Rewind(sample);  /* error is checked in recursion. */
-  return(read_more_data(sample));
-} /* read_more_data */
 
 void Sound::registerCommands(Console *console) {
   console->registerCommand(PLAY_SOUND, this);
--- src/Sound.h
+++ src/Sound.h
@@ -6,49 +6,31 @@
 #define SOUND_H 1
 
 #include <string>
+#include <map>
 
 #include <SDL/SDL.h>
-#include <SDL/SDL_sound.h>
+#include <SDL/SDL_mixer.h>
 
 #include "ConsoleObject.h"
     
-#define DEFAULT_DECODEBUF 16384
-#define DEFAULT_AUDIOBUF  4096
-
-#define PLAYSOUND_VER_MAJOR  0
-#define PLAYSOUND_VER_MINOR  1
-#define PLAYSOUND_VER_PATCH  5
-
 namespace Sear {
 
 class Console;
 	
 class Sound : public ConsoleObject {
 public:
-  Sound() {
-//    decoded_ptr = NULL;
-//    decoded_bytes = 0;
-//    predecode = 0;
-//    looping = 0;
-  }
+  Sound() {}
   ~Sound() {}
   void init();
   void shutdown();
   void playsound(const std::string &);
-
-  static void audio_callback(void *userdata, Uint8 *stream, int len);
-  static int read_more_data(Sound_Sample *sample);
-  static void finish_playback(Sound_Sample *sample);
+  Mix_Chunk *getSample(const std::string &file_name);
 
   void registerCommands(Console *console);
   void runCommand(const std::string &command, const std::string &args);
 private:
-  static Uint8 *decoded_ptr;
-  static Uint32 decoded_bytes;
-  static int predecode;
-  static int looping;
-  static bool done_flag;
   static const char * const PLAY_SOUND = "play_sound";
+  std::map<std::string, Mix_Chunk*> sound_map;
 
 };
   
