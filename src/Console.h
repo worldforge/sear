abdab4c5f0a3bb3ccd240fb492851ba5dc8273ae
Simon Goodall <simon@simongoodall.co.uk>
2002-08-20 12:57:59 +0000

    Updated comments for console code

--- src/Console.h
+++ src/Console.h
@@ -2,8 +2,8 @@
 // the GNU General Public License (See COPYING for details).
 // Copyright (C) 2001 - 2002 Simon Goodall, University of Southampton
 
-#ifndef _CONSOLE_H_
-#define _CONSOLE_H_ 1
+#ifndef CONSOLE_H
+#define CONSOLE_H 1
 
 #include <string>
 #include <list>
@@ -27,11 +27,14 @@
 #define SCREEN_MESSAGE  0x2
 
 namespace Sear {
-
+// Forward Declarations
 class ConsoleObject;
 class System;
 class Render;
 
+/**
+ * The console class handles the onscreen console, the screen messages and keeps track of all the console commands in use.
+ */ 
 class Console : public ConsoleObject {
 public:
   Console(System *system);
@@ -40,36 +43,77 @@ public:
   bool init();
   void shutdown();
  
-  void pushMessage(const std::string &, int, int);
-  void draw(const std::string &);
+  /**
+   * Add a message to the console and/or screen message queue
+   * message is the message string
+   * type is where to display the messgae: CONSOLE_MESSAGE and/or SCREEN_MESSAGE
+   * duration is how long the SCREEN_MESSAGE lasts before being removed
+   */ 
+  void pushMessage(const std::string &message, int type, int duration);
+  /**
+   * This method renders the console.
+   * command is the current command string
+   */ 
+  void draw(const std::string &command);
+  /**
+   * Toggles whether the console is visible or not
+   */ 
   void toggleConsole();
+  /**
+   * Returns whether the console is visible or not
+   */ 
   bool consoleStatus() { return showConsole; }
 
+  /**
+   * Registers a command with the console
+   * command is the command to register
+   * object is the originating object
+   */ 
   void registerCommand(const std::string &command, ConsoleObject *object);
+  /**
+   * This is the method the determines what object the pass the command onto
+   * command is the command string to process
+   */ 
   void runCommand(const std::string &command);
+
+  /**
+   * This is the ConsoleObject method.
+   * command is the command to run
+   * args is the commands arguments
+   */ 
   void runCommand(const std::string &command, const std::string &args);
 
 protected:
+  // Pair used to assign a duration to the screen message
   typedef std::pair<std::string, unsigned int> screenMessage;
 
-  void renderConsoleMessages(const std::string &);
+  /**
+   *  This render the console messges plus the current command string
+   *  command is the current command string
+   */ 
+  void renderConsoleMessages(const std::string &command);
+  /**
+   * This renders the screen messages
+   */ 
   void renderScreenMessages();
 
-  bool animateConsole;
-  bool showConsole;
-  int consoleHeight;
-  std::list<std::string> console_messages;
-  std::list<screenMessage> screen_messages;
-  int panel_id;
+  bool animateConsole; // Flag determining whether console is moving
+  bool showConsole; // flag to say whether console is visible/useable or not
+  int consoleHeight; // the height of the console. determined by number of messages allowed
+  std::list<std::string> console_messages; // Current console messages
+  std::list<screenMessage> screen_messages; // Current screen messages
+  int panel_id; // The texture id of the console's panel texture
   System *_system;
   Render *_renderer;
 
+  // Mapping of registered commands to assoicated object
   std::map<std::string, ConsoleObject*> _registered_commands;
  
+  // List of Console's console commands
   static const char * const TOGGLE_CONSOLE = "toggle_console";
   static const char * const LIST_CONSOLE_COMMANDS = "list_commands";
   
 };
 
 } /* namespace Sear */
-#endif /* _CONSOLE_H_ */
+#endif /* CONSOLE_H */
--- src/ConsoleObject.h
+++ src/ConsoleObject.h
@@ -2,19 +2,36 @@
 // the GNU General Public License (See COPYING for details).
 // Copyright (C) 2001 - 2002 Simon Goodall
 
-#ifndef _CONSOLEOBJECT_H_
-#define _CONSOLEOBJECT_H_ 1
+#ifndef CONSOLEOBJECT_H
+#define CONSOLEOBJECT_H 1
 
 #include <string>
 
 namespace Sear {
-
+/**
+ * The ConsoleObject is a interface used to allow objects to register commands
+ * with the console. Any object wishing to register a command, must implement 
+ *  this interface.
+ */ 
 class ConsoleObject {
 public:
+  /**
+   * Default constructor
+   */ 	
   ConsoleObject() {}
+
+  /**
+   * Default destructor
+   */ 
   virtual ~ConsoleObject() {}
+
+  /**
+   * This is the function that needs to be extended to use the console.
+   * command is a command that has been previously registered with the console
+   * args is the argument string that has been provided for the command
+   */ 
   virtual void runCommand(const std::string &command, const std::string &args) = 0;
 };
 
 } /* namespace Sear */
-#endif /* _CONSOLEOBJECT_H_ */
+#endif /* CONSOLEOBJECT_H */
