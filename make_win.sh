9f48ee50884a66a1f21ce9217cb5025af203f10f
Simon Goodall <simon@simongoodall.co.uk>
2002-12-12 16:44:15 +0000

    12th December 2002, Simon Goodall  * Added icon file and resource script for win32 executable  * Updated make_win32 script to build in app icon

--- make_win.sh
+++ make_win.sh
@@ -1,14 +1,18 @@
 #!/bin/sh -v
-GCC=/home/sg02r/cross-tools/i386-mingw32msvc/bin/gcc
-STRIP=/home/sg02r/cross-tools/i386-mingw32msvc/bin/strip
+CROSSTOOLS=/home/sg02r/cross-tools
+GCC=$CROSSTOOLS/i386-mingw32msvc/bin/gcc
+WINDRES=$CROSSTOOLS/bin/i386-mingw32msvc-windres
+STRIP=$CROSSTOOLS/i386-mingw32msvc/bin/strip
 
-DIR=/home/sg02r/cross-tools/lib
-DIR2=/home/sg02r/cross-tools/i386-mingw32msvc/lib
+DIR=$CROSSTOOLS/lib
+DIR2=$CROSSTOOLS/i386-mingw32msvc/lib
 
-LIBS="-L /home/sg02r/cross-tools/lib -lSDL -lSDL_mixer -lSDL_image -lopengl32"
+LIBS="-L $DIR -lSDL -lSDL_mixer -lSDL_image -lopengl32"
 
 STAT=" -lcal3d -lvarconf -leris -lsigc -l3ds -lAtlasNet -lAtlasFunky -lAtlasObjectsEntity -lAtlasObjectsOperation -lAtlasObjects -lAtlasMessage -lAtlasCodecs -lAtlas -lskstream -lwsock32 -lwfmath -leris -lglu32 -lopengl32  -lm -leris "
 
-$GCC -o sear.exe src/*.o common/*.o loaders/*.o terrain/*.o sky/*.o renderers/*.o gui/*.o $LIBS $STAT -lstdc++
+$WINDRES -i sear.rc -o sear_rc.o
+
+$GCC -o sear.exe src/*.o common/*.o loaders/*.o terrain/*.o sky/*.o renderers/*.o gui/*.o sear_rc.o $LIBS $STAT -lstdc++
 
 $STRIP sear.exe
