053e3c945322fb1e24198e83e23325d84efe0f11
Simon Goodall <simon@simongoodall.co.uk>
2003-02-25 21:57:25 +0000

    *** empty log message ***

--- loaders/Cal3d.cpp
+++ loaders/Cal3d.cpp
@@ -2,7 +2,7 @@
 // the GNU General Public License (See COPYING for details).
 // Copyright (C) 2001 - 2002 Simon Goodall, University of Southampton
 
-// $Id: Cal3d.cpp,v 1.29 2003-02-22 19:11:48 simon Exp $
+// $Id: Cal3d.cpp,v 1.30 2003-02-25 21:57:25 simon Exp $
 
 //#include <GL/gl.h>
 #include <SDL/SDL.h>
@@ -509,11 +509,16 @@ void Cal3d::renderMesh(bool useTextures, bool useLighting, bool select_mode)
         static int meshFaces[50000][3];
         int faceCount;
         faceCount = pCalRenderer->getFaces(&meshFaces[0][0]);
-
+        bool multitexture = false;
         // set the vertex and normal buffers
         if(!select_mode && (pCalRenderer->getMapCount() > 0) && (textureCoordinateCount > 0)) {
-          _render->switchTextureID((unsigned int)pCalRenderer->getMapUserData(0));
-          _render->renderElements(Graphics::RES_TRIANGLES, faceCount * 3, &meshFaces[0][0], &meshVertices[0][0], &meshTextureCoordinates[0][0], &meshNormals[0][0], false);
+          if (pCalRenderer->getMapCount() == 1) {
+            _render->switchTextureID((unsigned int)pCalRenderer->getMapUserData(0));
+	  } else {
+	    multitexture = true;
+            _render->switchMultiTextureID((unsigned int)pCalRenderer->getMapUserData(0), (unsigned int)pCalRenderer->getMapUserData(1));
+	  }
+          _render->renderElements(Graphics::RES_TRIANGLES, faceCount * 3, &meshFaces[0][0], &meshVertices[0][0], &meshTextureCoordinates[0][0], &meshNormals[0][0], multitexture);
 	} else {
           _render->renderElements(Graphics::RES_TRIANGLES, faceCount * 3, &meshFaces[0][0], &meshVertices[0][0], NULL, &meshNormals[0][0], false);
 	}
