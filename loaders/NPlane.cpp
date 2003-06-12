95151a325b8890b9587b463aecc472cb004a531f
Simon Goodall <simon@simongoodall.co.uk>
2003-06-12 19:38:54 +0000

    12th June 2003, Simon Goodall  * Fixed segfault caused by bad array indexing in NPlane loader

--- loaders/NPlane.cpp
+++ loaders/NPlane.cpp
@@ -2,7 +2,7 @@
 // the GNU General Public License (See COPYING for details).
 // Copyright (C) 2001 - 2002 Simon Goodall, University of Southampton
 
-// $Id: NPlane.cpp,v 1.17 2003-06-11 23:07:57 simon Exp $
+// $Id: NPlane.cpp,v 1.18 2003-06-12 19:38:54 simon Exp $
 
 #include "common/Utility.h"
 
@@ -46,61 +46,60 @@ bool NPlane::init(const std::string &type, unsigned int num_planes, float width,
   _type = type;
   _num_planes = num_planes;
   float rads_per_segment = WFMath::Pi / (float)num_planes;
-//  _vertex_data = (float*)malloc(8 * num_planes * 3 * sizeof(float));
-//  _normal_data = (float*)malloc(8 * num_planes * 3 * sizeof(float));
-//  _texture_data = (float*)malloc(8 * num_planes * 2 * sizeof(float));
   _vertex_data = new Vertex_3[8 * num_planes];
   _normal_data = new Normal[8 * num_planes];
   _texture_data = new Texel[8 * num_planes];
+  assert (_vertex_data && _normal_data && _texture_data);
   float in[3][3];
   float out[3];
+  std::cout << num_planes << std::endl;
   for (unsigned int i = 0; i < num_planes; i++) {
     float x = width * cos ((float)i * rads_per_segment) / 2.0f;
     float y = width * sin ((float)i * rads_per_segment) / 2.0f;
-    in[0][0] = _vertex_data[24 * i].x = x;
-    in[0][1] = _vertex_data[24 * i].y = y;
-    in[0][2] = _vertex_data[24 * i].z = 0.0f;
-    in[1][0] = _vertex_data[24 * i + 1].x = -x;
-    in[1][1] = _vertex_data[24 * i + 1].y = -y;
-    in[1][2] = _vertex_data[24 * i + 1].z = 0.0f;
-    in[2][0] = _vertex_data[24 * i + 2].x = -x;
-    in[2][1] = _vertex_data[24 * i + 2].y = -y;
-    in[2][2] = _vertex_data[24 * i + 2].z = height;
-    _vertex_data[24 * i + 3].x = x;
-    _vertex_data[24 * i + 3].y = y;
-    _vertex_data[24 * i + 3].z = height;
+    in[0][0] = _vertex_data[8 * i].x = x;
+    in[0][1] = _vertex_data[8 * i].y = y;
+    in[0][2] = _vertex_data[8 * i].z = 0.0f;
+    in[1][0] = _vertex_data[8 * i + 1].x = -x;
+    in[1][1] = _vertex_data[8 * i + 1].y = -y;
+    in[1][2] = _vertex_data[8 * i + 1].z = 0.0f;
+    in[2][0] = _vertex_data[8 * i + 2].x = -x;
+    in[2][1] = _vertex_data[8 * i + 2].y = -y;
+    in[2][2] = _vertex_data[8 * i + 2].z = height;
+    _vertex_data[8 * i + 3].x = x;
+    _vertex_data[8 * i + 3].y = y;
+    _vertex_data[8 * i + 3].z = height;
 
-    _vertex_data[24 * i + 4].x = x;
-    _vertex_data[24 * i + 4].y = y;
-    _vertex_data[24 * i + 4].z = height;
-    _vertex_data[24 * i + 5].x = -x;
-    _vertex_data[24 * i + 5].y = -y;
-    _vertex_data[24 * i + 5].z = height;
-    _vertex_data[24 * i + 6].x = -x;
-    _vertex_data[24 * i + 6].y = -y;
-    _vertex_data[24 * i + 6].z = 0.0f;
-    _vertex_data[24 * i + 7].x = x;
-    _vertex_data[24 * i + 7].y = y;
-    _vertex_data[24 * i + 7].z = 0.0f;
+    _vertex_data[8 * i + 4].x = x;
+    _vertex_data[8 * i + 4].y = y;
+    _vertex_data[8 * i + 4].z = height;
+    _vertex_data[8 * i + 5].x = -x;
+    _vertex_data[8 * i + 5].y = -y;
+    _vertex_data[8 * i + 5].z = height;
+    _vertex_data[8 * i + 6].x = -x;
+    _vertex_data[8 * i + 6].y = -y;
+    _vertex_data[8 * i + 6].z = 0.0f;
+    _vertex_data[8 * i + 7].x = x;
+    _vertex_data[8 * i + 7].y = y;
+    _vertex_data[8 * i + 7].z = 0.0f;
     calcNormal(in, out);
     for (unsigned int j = 0; j < 4; ++j) {
-      _normal_data[24 * i + 6 * j].x = out[0];
-      _normal_data[24 * i + 6 * j].y = out[1];
-      _normal_data[24 * i + 6 * j].z = out[2];
-      _normal_data[24 * i + 6 * j + 1].x = _normal_data[24 * i + 6 * j].x;
-      _normal_data[24 * i + 6 * j + 1].y = _normal_data[24 * i + 6 * j].y;
-      _normal_data[24 * i + 6 * j + 1].z = _normal_data[24 * i + 6 * j].z; 
+      _normal_data[8 * i + j].x = out[0];
+      _normal_data[8 * i + j].y = out[1];
+      _normal_data[8 * i + j].z = out[2];
+      _normal_data[8 * i + j + 4].x = -out[0];
+      _normal_data[8 * i + j + 4].y = -out[1];
+      _normal_data[8 * i + j + 4].z = -out[2];
     }
    
-    _texture_data[16 * i].s = 0.0f; _texture_data[16 * i].t = 0.0f;
-    _texture_data[16 * i + 1].s = 1.0f; _texture_data[16 * i + 1].t = 0.0f;
-    _texture_data[16 * i + 2].s = 1.0f; _texture_data[16 * i + 2].t = 1.0f;
-    _texture_data[16 * i + 3].s = 0.0f; _texture_data[16 * i + 3].t = 1.0f;
+    _texture_data[8 * i].s = 0.0f; _texture_data[8 * i].t = 0.0f;
+    _texture_data[8 * i + 1].s = 1.0f; _texture_data[8 * i + 1].t = 0.0f;
+    _texture_data[8 * i + 2].s = 1.0f; _texture_data[8 * i + 2].t = 1.0f;
+    _texture_data[8 * i + 3].s = 0.0f; _texture_data[8 * i + 3].t = 1.0f;
 
-    _texture_data[16 * i + 4].s =  0.0f; _texture_data[16 * i + 4].t =  1.0f;
-    _texture_data[16 * i + 5].s = 1.0f; _texture_data[16 * i + 5].t = 1.0f;
-    _texture_data[16 * i + 6].s = 1.0f; _texture_data[16 * i + 6].t = 0.0f;
-    _texture_data[16 * i + 7].s = 0.0f; _texture_data[16 * i + 7].t = 0.0f;
+    _texture_data[8 * i + 4].s =  0.0f; _texture_data[8 * i + 4].t =  1.0f;
+    _texture_data[8 * i + 5].s = 1.0f; _texture_data[8 * i + 5].t = 1.0f;
+    _texture_data[8 * i + 6].s = 1.0f; _texture_data[8 * i + 6].t = 0.0f;
+    _texture_data[8 * i + 7].s = 0.0f; _texture_data[8 * i + 7].t = 0.0f;
     
   }
   _initialised = true;
