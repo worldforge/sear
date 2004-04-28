3d5442e82fe091db97597c5ee93052027b697141
Simon Goodall <simon@simongoodall.co.uk>
2004-04-28 12:38:23 +0000

    28th April 2004, Simon Goodall  * Do not try and render acitveName string if its empty

--- renderers/GL.cpp
+++ renderers/GL.cpp
@@ -1150,6 +1150,7 @@ inline void GL::resetSelection() {
 }
 
 inline void GL::renderActiveName() {
+  if (active_name.empty()) return;
   glColor4fv(activeNameColour);
   print(x_pos, y_pos, active_name.c_str(), 1);
 }
