df67f36dac81d320affa52b89e403285ab4d5184
Simon Goodall <simon@simongoodall.co.uk>
2004-04-28 15:17:08 +0000

    28th April 2004, Simon Goodall  * Really fix the bad state stuff

--- src/ModelLoader.h
+++ src/ModelLoader.h
@@ -2,7 +2,7 @@
 // the GNU General Public License (See COPYING for details).
 // Copyright (C) 2001 - 2002 Simon Goodall
 
-// $Id: ModelLoader.h,v 1.21 2004-04-28 14:06:03 simon Exp $
+// $Id: ModelLoader.h,v 1.22 2004-04-28 15:17:09 simon Exp $
 
 #ifndef SEAR_MODELOADER_H
 #define SEAR_MODELOADER_H 1
@@ -39,11 +39,11 @@ public:
     model_record->offset_y = (double)model_config.getItem(model_id, ModelRecord::OFFSET_Y);
     model_record->offset_z = (double)model_config.getItem(model_id, ModelRecord::OFFSET_Z);
     
+    model_record->state = model_config.getItem(model_id, "state_num");
     model_record->state_name = (std::string)model_config.getItem(model_id, ModelRecord::STATE);
-    model_config.setItem(model_id, "state_num", model_record->state);
-    
+
+    model_record->select_state = model_config.getItem(model_id, "select_state_num");
     model_record->select_state_name = (std::string)model_config.getItem(model_id, ModelRecord::SELECT_STATE);
-    model_config.setItem(model_id, "select_state_num", model_record->select_state);
     
     model_record->model_by_type = model_config.getItem(model_id, ModelRecord::MODEL_BY_TYPE);
     model_record->outline = model_config.getItem(model_id, ModelRecord::OUTLINE);
