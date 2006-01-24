04e6f1a5f978dc3b05b13dce06a1468dc1df1f81
Al Riddoch <alriddoch@zepler.org>
2006-01-24 19:06:31 +0000

    2006-01-24  Al Riddoch  <alriddoch@zepler.org>
    	* guichan/Overlay.cpp, guichan/Overlay.cpp: New class to handle
    	  adding and removing widgets that overlay the game view, like
    	  status windows, and speech bubbles.

---
+++ guichan/Overlay.cpp
@@ -0,0 +1,19 @@
+// This file may be redistributed and modified only under the terms of
+// the GNU General Public License (See COPYING for details).
+// Copyright (C) 2006 Alistair Riddoch
+
+#include "Overlay.h"
+
+namespace Sear {
+
+Overlay * Overlay::m_instance = 0;
+
+Overlay::Overlay()
+{
+}
+
+Overlay::~Overlay()
+{
+}
+
+} // namespace Sear
