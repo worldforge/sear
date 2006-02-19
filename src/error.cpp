84621734f2f5f78279f3591fd2b97784c863fbcb
jmt <jmt>
2006-02-19 23:34:10 +0000

    2006-02-19  James Turner  <james@worldforge.org>
    	* src/error.cpp: On OS-X, use the Dialog Manager to display a
    	  standard alert via Carbon. Inside Macintosh is dead, but the
    	  toolbox will never die.

--- src/error.cpp
+++ src/error.cpp
@@ -4,6 +4,8 @@
 
 #ifdef WIN32
 #include <windows.h>
+#elif defined(__APPLE__)
+    #include <Carbon/Carbon.h>
 #else
 // Linunx Headers
 #include <stdio.h>
@@ -16,6 +18,21 @@ namespace Sear {
 void ErrorDialog(const std::string &msg) {
 #ifdef WIN32
   MessageBox(NULL, msg.c_str(), "Error", MB_ICONERROR | MB_OK);
+#elif defined(__APPLE__)
+    
+  DialogRef theAlert;
+  DialogItemIndex itemIndex;
+  CFStringRef cfmsg = CFStringCreateWithCString(kCFAllocatorDefault, 
+    msg.c_str(), kCFStringEncodingASCII);
+  
+  ::CreateStandardAlert(kAlertStopAlert,
+                CFSTR("Sear has experienced a serious error."),
+                cfmsg,
+                NULL, &theAlert);
+ 
+  ::RunStandardAlert (theAlert, NULL, &itemIndex); 
+  
+  CFRelease(cfmsg);
 #else
   printf("Error: %s\n", msg.c_str());
 #endif
