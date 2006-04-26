5ad87dc0fca1fcc75155bd415752a1bedf3a6c37
Simon Goodall <simon@simongoodall.co.uk>
2006-04-26 15:26:22 +0000

    2006-04-26  Simon Goodall  <sear@simongoodall.co.uk>
    	* common/Log.cpp: Alter log output slightly.
    
    	* BUGS, TODO, TODO.release: Updated

--- common/Log.cpp
+++ common/Log.cpp
@@ -2,7 +2,7 @@
 // the GNU General Public License (See COPYING for details).
 // Copyright (C) 2001 - 2006 Simon Goodall
 
-// $Id: Log.cpp,v 1.10 2006-02-14 17:25:53 simon Exp $
+// $Id: Log.cpp,v 1.11 2006-04-26 15:26:22 simon Exp $
 
 #include "Log.h"
 
@@ -30,7 +30,7 @@ void Log::writeLog(const std::string &msg, LogLevel level) {
     case LOG_INFO: type = "Info"; break;
     default: type="Unknown";
   }
-  printf("[%s]: %s\n", type.c_str(), msg.c_str());
+  printf("[%s] %s\n", type.c_str(), msg.c_str());
 }
 
 } /* namespace Sear */
