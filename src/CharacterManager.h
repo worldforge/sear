47373fb6edee5c967e931c46c3ce6afa8b70f6c8
Simon Goodall <simon@worldforge.org>
2009-07-13 18:49:38 +0100

    Added doc.
    2009-07-13  Simon Goodall  <simon@worldforge.org>
    
    	* src/CharacterManager.h: Added doc.

--- src/CharacterManager.h
+++ src/CharacterManager.h
@@ -31,6 +31,13 @@ namespace Sear {
 class Character;
 class Console;
 
+/** Class to manage multiple Character instances.
+ * The CharacterManager works around the Eris::Avatar lifecycle and manages
+ * character/avatar related tasks. A Character instance is created for each 
+ * Avatar object and is destroyed when the avatar is deactivated.
+ * Only a single character can be "active" at once. The active character is the
+ * focus for rendering and reciving user control commands.
+ */
 class CharacterManager : public ConsoleObject, public sigc::trackable {
 public:
   typedef std::map<std::string, Character*> CharacterMap;
@@ -49,44 +56,98 @@ public:
   void registerCommands(Console*);
   void runCommand(const std::string &, const std::string &);
 
+  /**
+   *  Returns a mapping between avatar id and Character objects.
+   */ 
   const CharacterMap &getCharacterMap() const {
     return m_character_map;
   }
 
+  /**
+   * Make the specified character object the active character.
+   * This object must be contained within the character map.
+   */ 
   bool setActiveCharacter(Character *character);
 
+  /**
+   * Return a pointer to the currently active character.
+   * This may be null.
+   */  
   Character *getActiveCharacter() const {
     return m_active_character;
   }
 
+  /**
+   * Signal fired when the active character changes. The character pointer may
+   * be null when there are no more active characters.
+   */ 
   sigc::signal<void, Character*> ActiveCharacterChanged;
 
+  /**
+   * Use the Eris::Account object to obtain Avatars.
+   *
+   */  
   void setAccount(Eris::Account *);
 
 private:
   bool m_initialised; 
 
+  /**
+   * Mapping between avatar id and Character instance.
+   */ 
   CharacterMap m_character_map;
 
+  /**
+   * Pointer to currently active character
+   */ 
   Character *m_active_character;
 
+  /**
+   * Callback for Avatar creation.
+   */ 
   void AvatarSuccess(Eris::Avatar *);
+
+  /**
+   * Callback for Avatar deactivation.
+   */ 
   void AvatarDeactivated(Eris::Avatar* av);
 
+  /**
+   * Callback when Avatar Entity has been created
+   */ 
   void GotCharacterEntity(Eris::Entity *e, Eris::Avatar *avatar);
 
-
+  /**
+   * Method to trigger character list retrieval from server..
+   */  
   int getCharacters();
+
+  /**
+   * Method to request character creation on the server
+   */ 
   int createCharacter(const std::string &, const std::string &, const std::string&, const std::string &);
+
+  /**
+   * Method to activate an existing character
+   */ 
   int takeCharacter(const std::string &);
 
+  /**
+   * Callback when an existing character object has been reported after a 
+   * getCharacters() call.
+   */ 
   void GotCharacterInfo(const Atlas::Objects::Entity::RootEntity&);
+
+  /**
+   * Callback fired when all existing characters have been reported after a
+   * getCharacters() call.
+   */
   void GotAllCharacters();
 
   bool m_take_first;
   Eris::Account *m_account;
 };
 
-} /* namespace Sear */
+} // namespace Sear 
 
-#endif /* SEAR_CHARACTERMANAGER_H */
+#endif // SEAR_CHARACTERMANAGER_H 
